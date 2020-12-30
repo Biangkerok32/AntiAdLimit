df004e9aa1c674996e903f2724dc1f5e3607044d
Biangkerok32 <48493593+Biangkerok32@users.noreply.github.com>
2020-12-30 20:20:09 +0700

    Update FanRewardListener.java

--- AntiAdLimit_Module/src/main/java/anti/ad/limit/fan/FanRewardListener.java
+++ AntiAdLimit_Module/src/main/java/anti/ad/limit/fan/FanRewardListener.java
@@ -8,8 +8,6 @@ public interface FanRewardListener {
 
     void onAdBanned();
 
-    void onRewardedVideoActivityDestroyed();
-
     void onRewardedVideoCompleted();
 
     void onLoggingImpression();
